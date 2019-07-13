36677e2923822849ff3c0feb168eee6a840a1e87
love-pixel <197wangming@gmail.com>
2019-07-13 17:34:30 +0800

    don't have defines, fix it in the next

---
+++ Step-2/Adding a Library/MathFunctions/sqrt.cxx
@@ -0,0 +1,20 @@
+#include "sqrt.h"
+#include <stdio.h>
+ 
+// a hack square root calculation using simple operations
+double mysqrt(double x)
+{
+	double result;
+	double delta;
+	result = x;
+ 
+	for (int i = 0; i < 10; ++i) {
+		if (result <= 0) {
+			result = 0.1;
+		}
+		delta = x - (result * result);
+		result = result + 0.5 * delta / result;
+		fprintf(stdout, "Computing sqrt of %g to be %g\n", x, result);
+	}
+	return result;
+}
--- Step-2/Adding a Library/tutorial.cxx
+++ Step-2/Adding a Library/tutorial.cxx
@@ -4,6 +4,8 @@
 
 #include "TutorialConfig.h"
 
+#include "mymath.h"
+
 int main(int argc, char* argv[])
 {
 	if(argc < 2)
@@ -14,6 +16,8 @@ int main(int argc, char* argv[])
 	}
 	double input_value = atof(argv[1]);
 	double output_value = sqrt(input_value);
-	fprintf(stdout,"The square root of %g is %g\n",input_value,output_value);
+	fprintf(stdout,"The square root of %g is %g [ math.h ]\n",input_value,output_value);
+	fprintf(stdout,"The square root of %g is %g [ mymath.h ]\n",input_value, output_value);
+	fprintf(stdout,"The multiply of %g and %d is %g [ mymath.h ]\n",input_value, 100, mul(input_value, 100));
 	return 0;
 }
